271f71d59f2fb1a66cd4842985411773b2e21a5e
amlane <twelvexstring@gmail.com>
2019-12-16 22:55:52 -0600

    header and tab components completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,25 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const headline = document.createElement("h1");
+  const temp = document.createElement("span");
+
+  header.appendChild(date);
+  header.appendChild(headline);
+  header.appendChild(temp);
+
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+  date.textContent = "SMARCH 28, 2019";
+  headline.textContent = "Lambda Times";
+  temp.textContent = "98°";
+
+  return header;
+}
+
+const container = document.querySelector(".header-container");
+container.appendChild(Header());
